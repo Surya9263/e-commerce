be7c6cfe213fa11eeb71166869a576902f65e2c8
Surya Srivastava <suryasrivastava724@gmail.com>
2023-01-18 22:33:26 +0530

    updated

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ProductCard = ({img,name,price}) => {
   return (
-    <div style={{width:"60%",marginTop:"22px",boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px"}}>
+    <div style={{width:"60%",marginTop:"25px",boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px"}}>
         <div>
             <p>{name}</p>
         </div>
